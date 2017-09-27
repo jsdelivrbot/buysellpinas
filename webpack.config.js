192630dd537a16042068a5b247eeaed4c8857fa2
Nico Sevilla <ns@pouchnation.com>
2017-09-27 11:41:10 +0800

    init repo

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,26 @@
 module.exports = {
-    entry: "./resources/assets/js/entry.js",
-    output: {
-        path: __dirname,
-        filename: "./resources/assets/js/bundle.js"
-    },
-    module: {
-       rules: [{
-            test: /\.css$/,
-            use: [
-           "style-loader",
-           "css-loader"
-            ]
-        }]
-    }
+  entry: [
+    './src/index.js'
+  ],
+  output: {
+    path: __dirname,
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [{
+      exclude: /node_modules/,
+      loader: 'babel',
+      query: {
+        presets: ['react', 'es2015', 'stage-1']
+      }
+    }]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: './'
+  }
 };
